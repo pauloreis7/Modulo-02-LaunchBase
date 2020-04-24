fadcc5276a28a0d2f5b0a96cc80fa62b44dcbd03
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2020-04-24 18:18:46 -0300

    Conclusão do projeto final
    Módulo totalmente concluido.Todas a funcionalidades e integrações com JS foram realizadas em todos os projeto e foram feitas todas as configurações e aperfeiçoamento no CSS e HTML em todos os projetos. Todos os projetos estão finalizados e concluidos com sucesso.

--- desafiosHTML/desafio_2-3/scripts.js
+++ desafiosHTML/desafio_2-3/scripts.js
@@ -1,5 +1,6 @@
 const modalFull = document.querySelector('.full_modal');
 const cards = document.querySelectorAll('.card')
+const modal = document.querySelector('.modal')
 
 for (let card of cards) {
     card.addEventListener("click", function () {
@@ -13,4 +14,14 @@ for (let card of cards) {
 modalFull.querySelector('.close_modal').addEventListener("click", function () {
     modalFull.classList.remove('active')
     modalFull.querySelector('iframe').src =""
+    
+    modal.classList.remove('maximized')
+})
+
+modalFull.querySelector('.expand_modal').addEventListener("click", function () {
+    if (modal.classList.contains('maximized')) {
+        modal.classList.remove('maximized')
+    } else {
+    modal.classList.add('maximized')
+    }
 })
